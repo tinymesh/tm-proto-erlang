5eff95031534b86bf81f2656b02faf779bc6a44f
Olav Frengstad <olav@fwt.no>
2012-07-25 15:53:20 +0200

    Return matchable tuplet for unserialize/1

--- src/tinymesh.erl
+++ src/tinymesh.erl
@@ -5,9 +5,9 @@
 -type touplet() :: {Key :: atom(), Value :: any()}.
 -type map()     :: [touplet(), ...].
 
--spec unserialize(binary()) -> List :: map().
+-spec unserialize(binary()) -> List :: {Status :: atom(), Data :: (map() | {atom(), any()})}.
 unserialize(<<Checksum:8/unsigned-integer, _/binary>> = Data) when byte_size(Data) /= Checksum ->
-	[{error, {checksum_mismatch, [{checksum, Checksum}, {size, byte_size(Data)}]}}];
+	{error, {checksum_mismatch, [{checksum, Checksum}, {size, byte_size(Data)}]}};
 
 unserialize(<<_Checksum:8/unsigned-integer,          % Message checksum
               SystemID:32/little-unsigned-integer,   % System wide ID
@@ -19,19 +19,19 @@ unserialize(<<_Checksum:8/unsigned-integer,          % Message checksum
               Latency:16/unsigned-integer,    % Time since msg creation (10ms)
               Payload/binary>>) ->
 	try
-		lists:flatten([[{system_id,      SystemID},
+		{ok, lists:flatten([[{system_id,      SystemID},
 		                {node_id,        UniqueID},
 		                {rssi,           RSSI},
 		                {jump_level,     NetworkLevel},
 		                {jump_count,     HopCounter},
 		                {msg_id,         Counter},
-		                {packet_latency, Latency}] | unserialize_payload(Payload)])
+		                {packet_latency, Latency}] | unserialize_payload(Payload)])}
 	catch
-			error: unknown_payload_type -> [{error, {unknown_payload_type, Payload}}]
+			error: unknown_payload_type -> {error, {unknown_payload_type, Payload}}
 	end;
 
 unserialize(Data) ->
-	[{error, {invalid_data, Data}}].
+	{error, {invalid_data, Data}}.
 
 -spec unserialize_payload(Payload :: binary()) -> map().
 unserialize_payload(<<16:8/unsigned-integer, 0:8, Payload/binary>>) ->
@@ -141,10 +141,10 @@ keyorerror(Key, List, Error) ->
 	-include_lib("eunit/include/eunit.hrl").
 
 	unserialize_checksum_mismatch_test() ->
-		[{error, {checksum_mismatch, _}}] = unserialize(<<123:8>>).
+		{error, {checksum_mismatch, _}} = unserialize(<<123:8>>).
 
 	unserialize_corrupt_msg_test() ->
-		[{error, {invalid_data, _}}] = unserialize(<<12, 54, 14 ,12, 15, 51,
+		{error, {invalid_data, _}} = unserialize(<<12, 54, 14 ,12, 15, 51,
 		                                             69, 15, 19, 51, 91, 15>>).
 
 	unserialize_payload_serial_test() ->
@@ -154,7 +154,7 @@ keyorerror(Key, List, Error) ->
 		?assert(base64:decode(SerialData) == Serial).
 
 	unserialize_payload_serial_no_constant_test() ->
-		[{error, {unknown_payload_type, _}}] = unserialize(<<20, 1:32, 2:32, 19,
+		{error, {unknown_payload_type, _}} = unserialize(<<20, 1:32, 2:32, 19,
 		                                                     1,  1,    1:16, 0:16,
 		                                                     16, "abc">>).
 
@@ -167,7 +167,7 @@ keyorerror(Key, List, Error) ->
 		?assert(Voltage == list_to_float(lists:flatten(io_lib:format("~.2f", [BinV * 0.03])))).
 
 	unserialize_payload_unknown_test() ->
-		[{error, {unknown_payload_type, _}}] = unserialize(<<20, 1:32, 2:32, 19,
+		{error, {unknown_payload_type, _}} = unserialize(<<20, 1:32, 2:32, 19,
 		                                                     1,  1,    1:16, 0:16,
 		                                                     99, "abc">>).
 

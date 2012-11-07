96b3300a7aa2215638352200d40d3baadda95ef7
Olav Frengstad <olav@fwt.no>
2012-11-07 19:34:57 +0100

    Make generator into 1 fun

--- test/protocol_tests.erl
+++ test/protocol_tests.erl
@@ -2,11 +2,8 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-unserialize_test_() ->
-	{generator, fun() ->
-		{ok, Buf} = file:read_file("../test/data/messages.b64"),
-		[ ?_test(begin
-			?assertMatch(
-				{ok, _}, tinymesh:unserialize( base64:decode(binary_to_list(X))))
-		  end) || <<_:88, X/binary>> <- binary:split(Buf,  <<10>>, [global])]
-	 end}.
+unserialize_test() ->
+	{ok, Buf} = file:read_file("../test/data/messages.b64"),
+	[ ?assertMatch(
+		{ok, _}, tinymesh:unserialize( base64:decode(binary_to_list(X))))
+			|| <<_:88, X/binary>> <- binary:split(Buf,  <<10>>, [global])].
